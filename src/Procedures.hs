1d525acc4f7ec7f88096277e33b523f78a249f58
dexterlb <dexterlb@qtrp.org>
2018-05-30 14:50:22 +0300

    parse procedure calls

--- src/Procedures.hs
+++ src/Procedures.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Procedures where
 
 import qualified NamedLambdas as NL
@@ -6,39 +9,121 @@ import Lambdas
 import Data.List (intercalate)
 import qualified Parser as P
 import Parser (Parser)
+import Control.Applicative (liftA2)
 
-data Call = Call String Context
+-- type Cache = HashMap (Identifier, Arity) UL.Expression
 
-instance Show Call where
-    show (Call f vars) = f ++ "(" ++ (intercalate ", " vars) ++ ")"
+type Identifier = String
+type Arity = Int
 
-data Procedure = Procedure Call NL.Expression
+data Declaration = Declaration String Context deriving (Eq)
+
+instance Show Declaration where
+    show (Declaration f vars) = f ++ "(" ++ (intercalate ", " vars) ++ ")"
+
+instance P.Parseable Declaration where
+    parser = fparser Declaration NL.varParser
+
+identifierParser = do
+    fh <- P.charOf "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ft <- P.manyOrNone $ P.charOf "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_"
+    return $ fh : ft
+
+fparser :: (Identifier -> [a] -> b) -> Parser a -> Parser b
+fparser constructor item = do
+    P.spaces
+    f <- identifierParser
+
+    vars <- P.fallback [] $ do
+        P.spaces
+        P.char '('
+        P.spaces
+        vars <- commaParser item
+        P.char ')'
+        return vars
+
+    return $ constructor f vars
+
+commaParser :: Parser a -> Parser [a]
+commaParser p = do
+    itemh <- p
+    itemt <- P.manyOrNone $ do
+        P.spaces
+        P.char ','
+        P.spaces
+        item <- p
+        P.spaces
+        return item
+    return $ itemh : itemt
+
+data Procedure = Procedure Declaration Expression
 
 instance Show Procedure where
     show (Procedure call body)
         = (show call) ++ " = " ++ (show body)
 
-instance P.Parseable Call where
-    parser = do
-        P.spaces
-        fh <- P.charOf "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        ft <- P.manyOrNone $ P.charOf "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_"
-        let f = fh : ft
+data Expression
+    = Variable      NL.Variable
+    | Application   Expression  Expression
+    | Lambda        NL.Variable Expression
+    | Call          Identifier  [Expression]
+    deriving (Eq)
 
-        P.spaces
-        P.char '('
-        P.spaces
-        varsh <- NL.varParser
-        varst <- P.manyOrNone $ do
+instance Show Expression where
+    show (Variable x)       = x
+    show (Application m n)  = "(" ++ (show m) ++ " " ++ (show n) ++ ")"
+    show (Lambda x m)       = "λ" ++ x ++ "" ++ (show m)
+    show (Call f terms)     = f ++ "(" ++ (intercalate ", " (map show terms)) ++ ")"
+
+instance P.Parseable Expression where
+    parser = expressionParser
+
+fromNamed :: NL.Expression -> Expression
+fromNamed (NL.Variable x)       = Variable x
+fromNamed (NL.Application m n)  = Application (fromNamed m) (fromNamed n)
+fromNamed (NL.Lambda x m)       = Lambda x (fromNamed m)
+
+expressionParser :: Parser Expression
+expressionParser = do
+    P.spaces
+
+    term <- P.unionl [
+        lambdaExprParser,
+        applicationExprParser,
+        varExprParser,
+        callExprParser,
+        bracedExprParser]
+
+    P.spaces
+    return term
+
+
+bracedExprParser :: Parser Expression
+bracedExprParser = do
+    P.char '('
+    term <- expressionParser
+    P.char ')'
+    return term
+
+varExprParser :: Parser Expression
+varExprParser = fmap Variable NL.varParser
+
+lambdaExprParser :: Parser Expression
+lambdaExprParser =
+    (P.unionl [P.string "lambda", P.string "\\", P.string "λ"])
+    *>
+    (P.concatenate Lambda NL.varParser expressionParser)
+
+applicationExprParser :: Parser Expression
+applicationExprParser = fmap leftAssoc $ P.many other
+    where
+        other = do
             P.spaces
-            P.char ','
+            term <- P.unionl [lambdaExprParser, varExprParser, callExprParser, bracedExprParser]
             P.spaces
-            var <- NL.varParser
-            P.spaces
-            return var
+            return term
 
-        let vars = varsh : varst
+        leftAssoc = foldl1 Application
 
-        P.char ')'
-
-        return $ Call f vars
+callExprParser :: Parser Expression
+callExprParser = fparser Call P.parser
